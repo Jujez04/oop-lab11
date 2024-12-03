e815a9ccb020447d6aa492d9e4e41de9ee3a1d1c
Justin Carideo <justin.carideo@studio.unibo.it>
2024-12-03 07:26:46 +0100

    MultiThreadedSumMatrix class works as expected by the test

--- 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -1,43 +1,57 @@
 package it.unibo.oop.workers02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MultiThreadedSumMatrix implements SumMatrix{
+/**
+ * Class that calculates the sum of the elements that belongs
+ * to a matrix using multithreading features.
+ */
+public class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int nThreads;
 
     public MultiThreadedSumMatrix(final int n) {
-        if(n < 0) {
+        if (n <= 0) {
             throw new IllegalArgumentException();
-        } 
+        }
         this.nThreads = n;
     }
 
+    /**
+     * @param matrix
+     *            an arbitrary-sized matrix
+     * @return the sum of its elements
+     */
     @Override
-    public double sum(double[][] matrix) {
-        final int rowPerThread = matrix.length > nThreads ? matrix.length / nThreads : 1;
+    public double sum(final double[][] matrix) {
+        final int rowPerThread = (int) Math.ceil((double) matrix.length / nThreads);
         final List<WorkerMatrix> workers = new ArrayList<>(nThreads);
-        for(int i = 0; i < nThreads; i++) {
+        for (int i = 0; i < nThreads; i++) {
             final int start = i * rowPerThread;
-            workers.add(new WorkerMatrix(matrix, start, start + rowPerThread));
+            final int endRow = Math.min(start + rowPerThread, matrix.length);
+            workers.add(new WorkerMatrix(matrix, start, endRow));
         }
-        for(final WorkerMatrix worker : workers) {
+        for (final WorkerMatrix worker : workers) {
             worker.start();
         }
         double res = 0.0;
-        for(final WorkerMatrix worker : workers) {
+        for (final WorkerMatrix worker : workers) {
             try {
                 worker.join();
                 res += worker.getResult();
-            } catch (Exception e) {
-                throw new IllegalStateException();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
             }
         }
-
         return res;
     }
-    
+
+    /**
+     * It creates threads that semplifies the calculation
+     * by summing the result of each row.
+     */
     private class WorkerMatrix extends Thread {
 
         private final double[][] matrix;
@@ -47,20 +61,30 @@ public class MultiThreadedSumMatrix implements SumMatrix{
 
         WorkerMatrix(final double[][] matrix, final int startRow, final int endRow) {
             super();
-            this.matrix = matrix;
+            this.matrix = new double[matrix.length][];
+            for (int i = 0; i < matrix.length; i++) {
+                this.matrix[i] = Arrays.copyOf(matrix[i], matrix[i].length);
+            }
             this.startRow = startRow;
             this.endRow = endRow;
         }
 
+        /**
+         * Run the worker.
+         */
         @Override
         public void run() {
-            for(int i = startRow; i < matrix.length && i < endRow; i++) {
-                for(int j = 0; j < matrix[i].length; j++) {
+            for (int i = startRow; i < endRow; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     this.result += this.matrix[i][j];
                 }
             }
         }
 
+        /**
+         * For getting the result.
+         * @return the result of each row
+         */
         private double getResult() {
             return this.result;
         }
